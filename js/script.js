4a38891d8a71a9f876c0fc5a40c48160fd014a13
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-21 17:51:59 -0400

    Creando lógica para obtener mokepon seleccionado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert("Probando, Probando!")
+function selectMokepon() {
+    let mokepon
+    let mokeponsAvailable = document.querySelectorAll("[name='mokepon']")
+    
+    for (let element of mokeponsAvailable){
+        if (element.checked) {
+            mokepon = element
+        }
+    }
+
+    return mokepon
+}
+
+// The program starts here:
+window.addEventListener('load', () => {
+    let selectMokeponButton = document.getElementById("button-select")
+    let mokeponSelected = selectMokeponButton.addEventListener("click", selectMokepon)
+})
