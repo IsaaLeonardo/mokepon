e121ad5e59bc10265b220e4e7e1e6a5fbe5b4624
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-21 19:58:47 -0400

    Creando una función para mostrar el nombre de los mokepones en el DOM

--- js/script.js
+++ js/script.js
@@ -3,15 +3,35 @@ function selectMokepon() {
     
     for (let mokepon of mokeponsAvailable){
         if (mokepon.checked) {
-            let mokeponNameSpan = document.getElementById('mokepon-player')
-            let mokeponName = document.querySelector(`[for='${mokepon.id}']`).innerHTML
-            
-            mokeponNameSpan.innerHTML = mokeponName
+            showMokepon(mokepon, "player")
+            selectMokeponOpponent(mokepon.id)
             return mokepon
         }
     }
     
     alert("Por favor, haz click en alguno de los mokepones o no podrás seguir jugando")
+}
+
+function selectMokeponOpponent(exceptionId){
+    let mokeponsList = document.querySelectorAll("[name='mokepon']")
+    let mokeponsAvailable
+
+    for (let mokeponIndex in mokeponsList) {
+        let mokepon = mokeponsList[mokeponIndex]
+        
+        if (mokepon.id == exceptionId) {
+            console.log(mokeponsList)
+            // mokeponsAvailable = mokeponsList.splice(mokeponIndex, 1)
+        }
+    }
+    console.log(mokeponsAvailable)
+    // showMokepon(mokepon, "opponent")
+}
+
+function showMokepon(mokepon, owner){
+    let mokeponNameSpan = document.getElementById(`mokepon-${owner}`)
+    let mokeponName = document.querySelector(`[for='${mokepon.id}']`).innerHTML
+    mokeponNameSpan.innerHTML = mokeponName
 }
 
 // The program starts here:
